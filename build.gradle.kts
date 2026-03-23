ef080a74fd76c53c9a8401c7688d75d118c703a3
Abrar Butt <abrarbuttmi7@gmail.com>
2026-03-23 21:28:51 +0500

    resolved conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,26 @@ repositories {
     mavenCentral()
 }
 
+val ktorVersion = "2.3.9"
+
 dependencies {
     implementation("org.apache.kafka:kafka-clients:4.0.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
     implementation("org.slf4j:slf4j-simple:2.0.12")
+
+    // Ktor WebSocket server
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-websockets:$ktorVersion")
 }
 
 application {
-    mainClass.set("main.MainKt")
+    mainClass.set("com.weatherflow.main.MainKt")
 }
+```
+
+---
+
+**Finally, create `local.properties`** in the project root:
+```
+hmac.secret=your_shared_secret_here
